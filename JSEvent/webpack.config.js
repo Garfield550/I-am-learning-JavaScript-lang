3551761a7cec6cbe004473ec1403b47936399b37
Garfield550 <garfield_550@outlook.com>
2017-03-27 20:05:02 +0800

    webpack configs

---
+++ JSEvent/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: './src/js/main.js',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /(\.css|\.scss)$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader", "sass-loader"]
+                })
+            }
+        ]
+    },
+    resolve: {
+        modules: [
+            "node_modules"
+        ],
+        extensions: [".js", ".css"]
+    },
+    plugins: [
+        new ExtractTextPlugin("main.css"),
+    ],
+    devtool: "cheap-module-eval-source-map",
+    devServer: {
+        compress: true,
+        port: 9000,
+        hot: true,
+        hotOnly: true,
+        open: true,
+        watchContentBase: true,
+        overlay: true
+    }
+}
