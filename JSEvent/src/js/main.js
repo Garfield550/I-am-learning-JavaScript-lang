4cbdeda1d0e9d4d19626a99239aa7997657968b0
Garfield550 <garfield_550@outlook.com>
2017-03-27 20:05:35 +0800

    js source code

---
+++ JSEvent/src/js/main.js
@@ -0,0 +1,3 @@
+import 'normalize.css';
+import '../scss/main.scss';
+import './modules/jump-ball';
---
+++ JSEvent/src/js/modules/jump-ball.js
@@ -0,0 +1,5 @@
+let canvas = document.getElementById("jump-ball");
+let context = canvas.getContext("2d");
+
+context.fillStyle = "green";
+context.fillRect(10, 10, 100, 100);
