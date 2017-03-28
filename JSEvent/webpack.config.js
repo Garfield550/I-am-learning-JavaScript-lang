b065293074d2f145d05bf5f86977147cd306f880
Garfield550 <garfield_550@outlook.com>
2017-03-28 08:17:34 +0800

    webpack.config: add webpack plugin

--- JSEvent/webpack.config.js
+++ JSEvent/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: './src/js/main.js',
@@ -30,6 +31,7 @@ module.exports = {
         extensions: [".js", ".css"]
     },
     plugins: [
+        new webpack.HotModuleReplacementPlugin(),
         new ExtractTextPlugin("main.css"),
     ],
     devtool: "cheap-module-eval-source-map",
