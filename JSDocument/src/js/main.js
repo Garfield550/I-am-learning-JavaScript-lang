752c21ad1e93fb29a6f746e93bbc6a6e9006a6bc
Garfield550 <garfield_550@outlook.com>
2017-04-21 09:20:06 +0800

    JSDocument: source code

---
+++ JSDocument/src/js/main.js
@@ -0,0 +1,14 @@
+document.fgColor = '#F4F4F4';
+document.bgColor = '#767676';
+document.linkColor = '#137697';
+document.alinkColor = '#248708';
+document.vlinkColor = '#359819';
+
+function onMouseOver() {
+    document.fgColor = '#767676';
+    document.bgColor = '#F4F4F4';
+}
+
+function onClick() {
+    document.fgColor = 'blue';
+}
